4bfe32ade043ef839668095cfbf037e4527c9e11
Niccocande <92431938+Niccocande@users.noreply.github.com>
2021-11-04 16:43:19 +0100

    Create es_6.1.cpp

---
+++ es_6.1.cpp
@@ -0,0 +1,23 @@
+//massimo in un vettore
+#include <iostream>
+using namespace std;
+
+double massimo (const double vett[], unsigned int n) {
+    if (n == 0)
+        return 0;
+    double max = 0.0;
+    for (int i = 1; i < n; i++) {
+        if (vett[i] > max)
+            max = vett[i];
+    }
+        return max;
+    
+}
+
+ int main (){
+    const int DIM=6;
+    double vett[6]= {1.2, 4.5, 2.4, 8.4, -3.14, -3.14};
+    cout<< massimo (vett,DIM);
+    return 0;
+
+}
