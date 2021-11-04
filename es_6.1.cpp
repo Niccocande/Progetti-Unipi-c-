7e8143f8ebbe13225708f6cf9498776ef1eb6e66
Niccocande <92431938+Niccocande@users.noreply.github.com>
2021-11-04 16:55:34 +0100

    Update es_6.1.cpp

--- es_6.1.cpp
+++ es_6.1.cpp
@@ -5,18 +5,18 @@ using namespace std;
 double massimo (const double vett[], unsigned int n) {
     if (n == 0)
         return 0;
-    double max = 0.0;
-    for (int i = 1; i < n; i++) {
+    double max = vett[0];
+    for (int i = 0; i < n; ++i) {
         if (vett[i] > max)
             max = vett[i];
     }
         return max;
-    
+
 }
 
  int main (){
     const int DIM=6;
-    double vett[6]= {1.2, 4.5, 2.4, 8.4, -3.14, -3.14};
+    double vett[6]= {9.0, 4.5, 2.4, 8.4, -3.14, -3.14};
     cout<< massimo (vett,DIM);
     return 0;
 
